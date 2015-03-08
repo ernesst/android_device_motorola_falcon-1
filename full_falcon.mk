96606ef491ee9a7e170bb122ac9932cb491de7ab
Louis Popi <theh2o64@gmail.com>
2015-03-08 09:32:51 +0100

    falcon : Fix lunch

--- full_falcon.mk
+++ full_falcon.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2014 The CyanogenMod Project
+# Copyright (C) 2015 The h2o64 Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,6 +12,17 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+#
 
-PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/full_falcon.mk
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from falcon device
+$(call inherit-product, device/motorola/falcon/falcon.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := falcon
+PRODUCT_NAME := full_falcon
+PRODUCT_BRAND := motorola
+PRODUCT_MODEL := falcon
+PRODUCT_MANUFACTURER := motorola
